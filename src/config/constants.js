d5fdeb04033539af52624f5e7de7bf124f1f7c7f
wooyoung6685 <wooyoung6685@gmail.com>
2023-03-21 15:38:52 +0900

    port수정

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,3 @@
-//export const API_URL = process.env.NODE_ENV === "production" ? "https://port-0-react-db-3xcah2glbi42tno.gksl2.cloudtype.app" : "http://localhost:8080";
+export const API_URL = process.env.NODE_ENV === "production" ? "https://port-0-mangoserver-nx562olfgbsppu.sel3.cloudtype.app" : "http://localhost:8080";
 
-export const API_URL = "http://localhost:8080";
+//export const API_URL = "http://localhost:8080";
